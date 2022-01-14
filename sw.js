2463075576af1a9535f4cbe3bcb46eedabce43f4
AdiThakker <Aditya.Thakker@gmail.com>
2022-01-14 08:26:01 -0500

    updates to image and cache

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 ---
 layout: null
 ---
-var CACHE_NAME = "saikiransripada-{{site.time | date: '%Y%m%d%H%M%S'}}";
+var CACHE_NAME = "adithakker-{{site.time | date: '%Y%m%d%H%M%S'}}";
 
 self.addEventListener("install", function(e) {
   e.waitUntil(
